0c2490a2fa9a516dac777db7b40baf514fff3f90
Ale Arredondo <alejandrarredondo09@gmail.com>
2024-10-28 07:10:33 -0600

    UX design
    Signed-off-by: Ale Arredondo <alejandrarredondo09@gmail.com>

--- plant/urls.py
+++ plant/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from sensor_data import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sensor_data.urls')),
+    path('', views.main, name='main'),
 ]
--- sensor_data/views.py
+++ sensor_data/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from .models import TempSensor
-from .sensor import *
+from .sensor import get_temperature  # Import get_temperature function
 
 def main(request):
-    
+    # Get the latest temperature data
     temperature = get_temperature()
-    temperature_value = temperature['value'] 
-    timestamp = temperature['timestamp'] 
+    temperature_value = temperature['value']
+    timestamp = temperature['timestamp']
 
+    # Save to the database
     new_registry = TempSensor(value=temperature_value, timestamp=timestamp)
     new_registry.save()
-    
+
+    # Get all temperature records
     registries = TempSensor.objects.all()
     return render(request, 'main.html', {'registries': registries})
